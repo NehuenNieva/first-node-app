6f15f621557a72728b682cbbbd76669994afa928
Nehuen Nieva <nehuennieva@hotmail.com>
2020-05-20 10:11:45 -0300

    Segundo Commit, Se agrago funciones

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-
 //let pathObj = path.parse(__filename);
 
 //logger.log(pathObj);
@@ -17,20 +16,39 @@ console.log(" total memory: ", totalMemory);
 
 let rootDir = fs.readdirSync("./");
 
-
-fs.readdir("./",(err,files)=>{
+//lee todo los archivos de un Dir
+fs.readdir("/home/devlop/Escritorio/",(err,files)=>{
     (err) ? console.log(err) : console.log("async",files);
-    
-    
+});
+
+//lee el contenido de un archivo
+fs.readFile('App.js','utf-8', (err, data) => {
+    (err) ? console.log('error: ', err) : console.log(data);
+  });
+
+  //Ordena los Dir alreves
+  fs.readdir("./",(err,files)=>{
+    (err) ? console.log(err) : console.log("Archivos Ordenados alreves",files.reverse())
+});
+
+
+//busca los directoriosp por la primera letra
+fs.readdir("./",(err,files)=>{
+    (err) ? console.log(err) : console.log("las encontre"),buscarPalabra(files,'A')
 });
 
 console.log("Sync",rootDir);
 
+//para buscar una palabra y por su primer caracter
+function buscarPalabra(palabra,letra){
+    var cont = 0;
+for(var i =0 ; i < palabra.length ;  i++){
 
+    if(palabra[i] && palabra[i].charAt(0)===letra){
+        cont++;
+    };
+};
 
-
-
-
-
-
+console.log(cont);
+}
 
